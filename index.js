727602326d06299503fa56eda0f5965d3e697ea9
inthemonzon <inthemonzon@yahoo.com>
2021-06-20 15:47:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+//const name = []
+//const event = 'surprise'
+
+function writeCards(names, event) {
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return newArray;
+}
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
